89f180cfe8b97c2192067a515c308b29179a97ba
zhengxingcheng <xingcheng.zheng@gmail.com>
2016-07-30 20:49:05 +0800

    first commit
    代码是借鉴fis-parser-bdtmpl的

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var html2js = require('html2js');
+var css2js = require('fis3-preprocessor-css2js');
+
+module.exports = function(content, file, conf){
+    if (!file.amdPlugin) return;
+    switch (file.amdPlugin) {
+        case 'text':
+            content = html2js(content, {
+                mode: 'compress',
+                wrap: 'commonjs'
+            });
+            break;
+        case 'css':
+            content = css2js.processCss(content, {
+                template: 'requirejs_runner'
+            });
+        default:
+            break;
+    }
+
+    return content;
+};
